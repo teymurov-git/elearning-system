785c49404f227d352cd404fc9240a03aa50351f1
Teymurlu Yusif <teymurluyusfi555@gmail.com>
2025-12-14 15:50:30 +0400

    updated models

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,40 @@
 from django.contrib import admin
-from .models import User, UserAdmin, Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
+from django.urls import reverse
+from .models import User, Group
 
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Group)
+class UserAdminCustom(BaseUserAdmin):
+    list_display = ('first_name', 'last_name', 'phone', 'email', 'group', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email', 'phone')
+    list_filter = ('group', 'is_staff', 'is_superuser', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'group')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'phone', 'group'),
+        }),
+    )
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user_count_link')
+    search_fields = ('name',)
+    
+    def user_count_link(self, obj):
+        count = obj.users.count()
+        if count > 0:
+            url = reverse('admin:account_user_changelist') + f'?group__id__exact={obj.id}'
+            return format_html('<a href="{}">{} istifadəçi</a>', url, count)
+        return '0 istifadəçi'
+    user_count_link.short_description = 'İstifadəçilər'
+
+
+admin.site.register(User, UserAdminCustom)
+admin.site.register(Group, GroupAdmin)
--- account/models.py
+++ account/models.py
@@ -1,20 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.admin import ModelAdmin
 
 # Create your models here.
 
 class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
     email = models.EmailField("email address")
+    group = models.ForeignKey('Group', on_delete=models.SET_NULL, null=True, blank=True, related_name='users', verbose_name='Group')
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name + ' ' + self.phone + ' ' + self.email
-    
-
-class UserAdmin(ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone', 'email', 'is_staff')
-    search_fields = ('first_name', 'last_name', 'email', 'phone')
 
 
 class Group(models.Model):
