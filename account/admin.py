06acf7b0d7a8ff095b93a004b58063bfc058e016
Teymurlu Yusif <teymurluyusfi555@gmail.com>
2025-12-14 15:43:48 +0400

    updated models

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User, UserAdmin, Group
 
-# Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Group)
--- system/settings.py
+++ system/settings.py
@@ -104,6 +104,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+AUTH_USER_MODEL = 'account.User'
+
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
